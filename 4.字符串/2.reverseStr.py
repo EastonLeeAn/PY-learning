fb790b05bddf7a1f40a0f9ba66915a212931f95f
EastonLeeAn <physicsquantum163@gmail.com>
2024-06-19 22:07:34 +0800

    1

--- 4.字符串/2.reverseStr.py
+++ 4.字符串/2.reverseStr.py
@@ -19,3 +19,21 @@ class Solution:
         if nxt[-1] != -1 and len(s) % (len(s) - (nxt[-1] + 1)) == 0:
             return True
         return False
+
+    def build_partial_match_table(pattern):
+        m = len(pattern)
+        lps = [0] * m
+        length = 0
+        i = 1
+        while i < m:
+            if pattern[i] == pattern[length]:
+                length += 1
+                lps[i] = length
+                i += 1
+            else:
+                if length != 0:
+                    length = lps[length - 1]
+                else:
+                    lps[i] = 0
+                    i += 1
+        return lps
--- 4.字符串/7.repeatedSubstringPattern.py
+++ 4.字符串/7.repeatedSubstringPattern.py
@@ -13,6 +13,13 @@
 
 
 class Solution:
+    def repeated1(self, strs):
+        t = strs + strs
+        t = t[1:-1]
+        if strs in t:
+            return True  # 如果s是t的子串
+        return False
+
     def repeatedSubstringPattern(self, s: str) -> bool:
         n = len(s)
         if n <= 1:
@@ -26,3 +33,48 @@ class Solution:
                     return True
 
         return False
+
+
+## KMP 算法
+class Solution1:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        if len(s) == 0:
+            return False
+        nxt = [0] * len(s)
+        self.getNext(nxt, s)
+        if nxt[-1] != -1 and len(s) % (len(s) - (nxt[-1] + 1)) == 0:
+            return True
+        return False
+
+    def getNext(self, nxt, s):
+        nxt[0] = -1
+        j = -1
+        for i in range(1, len(s)):
+            while j >= 0 and s[i] != s[j + 1]:
+                j = nxt[j]
+            if s[i] == s[j + 1]:
+                j += 1
+            nxt[i] = j
+        return nxt
+
+
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        if len(s) == 0:
+            return False
+        nxt = [0] * len(s)
+        self.getNext(nxt, s)
+        if nxt[-1] != 0 and len(s) % (len(s) - nxt[-1]) == 0:
+            return True
+        return False
+
+    def getNext(self, nxt, s):
+        nxt[0] = 0
+        j = 0
+        for i in range(1, len(s)):
+            while j > 0 and s[i] != s[j]:
+                j = nxt[j - 1]
+            if s[i] == s[j]:
+                j += 1
+            nxt[i] = j
+        return nxt
